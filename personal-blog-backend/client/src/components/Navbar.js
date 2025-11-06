89a173090cc8cd7fc0be8cd5ea0476917fbaedcc
Akash-Kumbhar-Ak <akashkumbhar3112@gmail.com>
2025-11-06 12:58:37 +0530

    Remove Admin Login button from navbar

--- personal-blog-backend/client/src/components/Navbar.js
+++ personal-blog-backend/client/src/components/Navbar.js
@@ -15,9 +15,6 @@ const Navbar=()=>{
                     <li>
                         <Link to="/" className="nav-link">Home</Link>
                     </li>
-                    <li>
-                        <Link to="/admin/login" className="nav-link">Admin Login</Link>
-                    </li>
                 </ul>
             </div>
         </nav>
